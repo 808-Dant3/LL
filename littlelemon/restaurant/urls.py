4f4be7d2c482df9eac9a16325c9db1ea1ac123e9
Dante <arriaga.02.17.98@gmail.com>
2023-01-11 19:04:32 -0800

    one last fix

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', index, name='index'),
     path('menu/', menuview.as_view()),
 ]
